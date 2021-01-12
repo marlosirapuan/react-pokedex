d7dd82eae55b0612901365664cf05d64e56decf3
Marlos <marlos.irapuan@gmail.com>
2021-01-12 19:43:05 -0300

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import store from 'store/store'
 import App from './App'
 
 test('renders the app', () => {
-  const { getByLabelText } = render(<App />)
+  const { getByLabelText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+
   const containerAppElement = getByLabelText(/container/i)
   const containerHeaderElement = getByLabelText(/header/i)
   const containerSearchElement = getByLabelText(/search/i)
